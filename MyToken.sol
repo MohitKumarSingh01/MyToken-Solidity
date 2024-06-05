d85d187ff7b1509bb453507e30d12302a0d95b87
Mohit Kumar Singh <159576282+MohitKumarSingh01@users.noreply.github.com>
2024-06-05 16:07:00 +0530

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "Ether";
+    string public tokenAbbrv = "ETH";
+    uint public totalsupply = 1000000;
+
+    // Mapping to track the token balance of each address
+    mapping(address => uint) public tokenBalances;
+
+    // Function to mint new tokens and assign them to a specified address
+    function mint(address recipient, uint amount) public {
+        totalsupply += amount;
+        tokenBalances[recipient] += amount;
+    }
+
+    // Function to burn tokens from a specified address, reducing the total supply
+    function burn(address account, uint amount) public {
+        require(tokenBalances[account] >= amount, "Not enough tokens to burn");
+        totalsupply -= amount;
+        tokenBalances[account] -= amount;
+    }
+}
